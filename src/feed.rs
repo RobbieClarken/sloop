8756aa09d40526bfd3963e9032d206a0368d76bb
RobbieClarken <robbie.clarken@gmail.com>
2019-10-14 20:32:13 +1100

    Ensure feed specifies itunes xml namespace

--- src/feed.rs
+++ src/feed.rs
@@ -1,11 +1,25 @@
+use rss::extension::itunes::{ITunesChannelExtensionBuilder, NAMESPACE};
 use rss::ChannelBuilder;
+use std::collections::HashMap;
 use std::io::prelude::*;
 
 pub struct FeedGenerator;
 
 impl FeedGenerator {
     pub fn generate_for_dir<W: Write>(&self, _dir: &str, mut writer: W) {
-        let channel = ChannelBuilder::default().build().unwrap();
+        let namespaces: HashMap<String, String> = [("itunes".to_string(), NAMESPACE.to_string())]
+            .iter()
+            .cloned()
+            .collect();
+        let itunes_ext = ITunesChannelExtensionBuilder::default()
+            .block("Yes".to_string())
+            .build()
+            .unwrap();
+        let channel = ChannelBuilder::default()
+            .namespaces(namespaces)
+            .itunes_ext(itunes_ext)
+            .build()
+            .unwrap();
         channel.pretty_write_to(&mut writer, b' ', 2).unwrap();
     }
 }
@@ -14,14 +28,25 @@ impl FeedGenerator {
 mod tests {
     use super::*;
 
+    #[macro_export]
+    macro_rules! assert_contains {
+        ($haystack:expr, $needle:expr) => {{
+            assert!(
+                $haystack.contains($needle),
+                format!(
+                    "expected string to contain \"{}\", got:\n\n{}\n\n",
+                    $needle, $haystack
+                )
+            );
+        }};
+    }
+
     #[test]
     fn generates_xml_for_dir() {
         let mut buffer = Vec::new();
         (FeedGenerator {}).generate_for_dir("test_fixtures/dir1/", &mut buffer);
         let feed = String::from_utf8(buffer).unwrap();
-        assert!(
-            feed.starts_with("<rss"),
-            format!("expected feed to start with \"<rss\", got:\n\n{}\n\n", feed)
-        );
+        assert_contains!(feed, "xmlns:itunes");
+        assert_contains!(feed, "<itunes:block>Yes</itunes:block>");
     }
 }
